fba1b7d7847cb91fdfed3866e000ced15c6071cf
AbSam06 <Samyabd2019@gmail.com>
2025-02-21 13:42:55 -0500

    first commit

---
+++ src/main/java/com/mycompany/exampoofinal/ExamPooFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exampoofinal;
+
+/**
+ *
+ * @author 2417011
+ */
+public class ExamPooFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
